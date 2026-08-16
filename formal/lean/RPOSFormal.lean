2ad954fc8d6267a9a5804e92ce1b44314461633f
Akihisa Ono / 小野 昭久（ PN: Yutori Komeiji / 古明地ゆとり ）ID: dantarg <akihisa.ono.0118@gmail.com>
2026-08-16 11:38:10 +0900

    formal: remove superseded Lake root module name

--- formal/lean/RPOSFormal.lean
+++
@@ -1,9 +0,0 @@
--- Copyright (c) 2026 Akihisa Ono
--- SPDX-License-Identifier: MIT
--- RPOS-SOURCE-LANG: en
-import RPOSState
-import RPOSReachability
-import RPOSEvidenceBoundary
-import RPOSPacketBoundary
-import RPOSOperationalBoundary
-import RPOSTransparencyBoundary
